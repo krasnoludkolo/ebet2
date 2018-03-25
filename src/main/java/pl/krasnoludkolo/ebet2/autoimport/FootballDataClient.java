58699ce001b94d5986ea63a34fa5d59d2a9e08bf
janek@projmen.pl <janek@projmen.pl>
2018-03-26 00:45:36 +0200

    Autoimport module refactor

--- src/main/java/pl/krasnoludkolo/ebet2/autoimport/FootballDataClient.java
+++ src/main/java/pl/krasnoludkolo/ebet2/autoimport/FootballDataClient.java
@@ -32,23 +32,27 @@ class FootballDataClient implements ExternalSourceClient {
         List<MatchInfo> matchInfos = List.empty();
         for (int i = 0; i < fixtures.length(); i++) {
             JSONObject fixture = fixtures.getJSONObject(i);
-            String homeTeamName = fixture.getString("homeTeamName");
-            String awayTeamName = fixture.getString("awayTeamName");
-            int round = fixture.getInt("matchday");
-            String status = fixture.getString("status");
-
-            boolean finished = status.equals("FINISHED");
-            MatchResult result = MatchResult.NOT_SET;
-            if (finished) {
-                JSONObject matchResult = fixture.getJSONObject("result");
-                int goalsHomeTeam = matchResult.getInt("goalsHomeTeam");
-                int goalsAwayTeam = matchResult.getInt("goalsAwayTeam");
-                result = getMatchResult(goalsHomeTeam, goalsAwayTeam);
-            }
-            MatchInfo matchInfo = new MatchInfo(homeTeamName, awayTeamName, round, finished, result);
+            MatchInfo matchInfo = createMatchInfo(fixture);
             matchInfos = matchInfos.append(matchInfo);
         }
         return matchInfos;
+    }
+
+    private MatchInfo createMatchInfo(JSONObject fixture) {
+        String homeTeamName = fixture.getString("homeTeamName");
+        String awayTeamName = fixture.getString("awayTeamName");
+        int round = fixture.getInt("matchday");
+        String status = fixture.getString("status");
+
+        boolean finished = status.equals("FINISHED");
+        MatchResult result = MatchResult.NOT_SET;
+        if (finished) {
+            JSONObject matchResult = fixture.getJSONObject("result");
+            int goalsHomeTeam = matchResult.getInt("goalsHomeTeam");
+            int goalsAwayTeam = matchResult.getInt("goalsAwayTeam");
+            result = getMatchResult(goalsHomeTeam, goalsAwayTeam);
+        }
+        return new MatchInfo(homeTeamName, awayTeamName, round, finished, result);
     }
 
     private MatchResult getMatchResult(int g1, int g2) {
