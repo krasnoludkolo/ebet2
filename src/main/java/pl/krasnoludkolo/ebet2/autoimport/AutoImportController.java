2bf793d2adeb99bd34a303eae86cbc2fb1f4ab21
janek@projmen.pl <janek@projmen.pl>
2018-03-29 00:32:29 +0200

    Endpoint to create import configuration

---
+++ src/main/java/pl/krasnoludkolo/ebet2/autoimport/AutoImportController.java
@@ -0,0 +1,33 @@
+package pl.krasnoludkolo.ebet2.autoimport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.krasnoludkolo.ebet2.autoimport.api.ExternalSourceConfiguration;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Controller("/api")
+class AutoImportController {
+
+    private AutoImportFacade facade;
+
+    @Autowired
+    AutoImportController(AutoImportFacade facade) {
+        this.facade = facade;
+    }
+
+    @PostMapping("/autoimport")
+    public ResponseEntity<UUID> initializeLeague(@RequestBody Map<String, String> configMap) {
+        ExternalSourceConfiguration config = new ExternalSourceConfiguration();
+        configMap.forEach(config::putParameter);
+        String clientShortcut = configMap.get("shortcut");
+        UUID uuid = facade.initializeLeague(config, clientShortcut);
+        return new ResponseEntity<>(uuid, HttpStatus.CREATED);
+    }
+
+}
