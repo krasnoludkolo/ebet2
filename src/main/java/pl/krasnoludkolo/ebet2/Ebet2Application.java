ae7eeb9ef50976f4526cf5b52bb20b46950f4d01
janek@projmen.pl <janek@projmen.pl>
2018-04-22 17:31:49 +0200

    Remove heroku test

--- src/main/java/pl/krasnoludkolo/ebet2/Ebet2Application.java
+++ src/main/java/pl/krasnoludkolo/ebet2/Ebet2Application.java
@@ -4,18 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.time.LocalDateTime;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 @SpringBootApplication
 @EnableScheduling
 public class Ebet2Application {
 
     public static void main(String[] args) {
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(4);
-        scheduledExecutorService.scheduleAtFixedRate(() -> System.out.println(LocalDateTime.now().toString() + " Still alive :D"), 1, 1, TimeUnit.SECONDS);
         SpringApplication.run(Ebet2Application.class, args);
     }
 
