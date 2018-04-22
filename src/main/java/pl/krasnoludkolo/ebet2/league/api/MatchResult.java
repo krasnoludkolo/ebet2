71cf6703e661035a7ed5f0d3287e000acfc11843
janek@projmen.pl <janek@projmen.pl>
2018-04-22 16:39:22 +0200

    Add new external source

--- src/main/java/pl/krasnoludkolo/ebet2/league/api/MatchResult.java
+++ src/main/java/pl/krasnoludkolo/ebet2/league/api/MatchResult.java
@@ -2,6 +2,15 @@ package pl.krasnoludkolo.ebet2.league.api;
 
 public enum MatchResult {
 
-    NOT_SET, HOST_WON, GUEST_WON, DRAW
+    NOT_SET, HOST_WON, GUEST_WON, DRAW;
 
+    public static MatchResult fromResult(int hostGloas, int guestGoals) {
+        if (hostGloas > guestGoals) {
+            return MatchResult.HOST_WON;
+        } else if (hostGloas < guestGoals) {
+            return MatchResult.GUEST_WON;
+        } else {
+            return MatchResult.DRAW;
+        }
+    }
 }
