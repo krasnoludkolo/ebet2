4f1719cc03c418f61af216889e4b16951ede0989
janek@projmen.pl <janek@projmen.pl>
2018-05-04 12:11:26 +0200

    Change controller type

--- src/main/java/pl/krasnoludkolo/ebet2/external/AutoUpdateController.java
+++ src/main/java/pl/krasnoludkolo/ebet2/external/AutoUpdateController.java
@@ -2,14 +2,14 @@ package pl.krasnoludkolo.ebet2.external;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@Controller()
+@RestController
 @RequestMapping("api")
 class AutoUpdateController {
 
