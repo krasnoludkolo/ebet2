541f2575f8f3e46a7e52cd806a4df3d989e113da
janek@projmen.pl <janek@projmen.pl>
2018-05-06 22:37:15 +0200

    Update allowedOrigins

--- src/main/java/pl/krasnoludkolo/ebet2/config/WebConfig.java
+++ src/main/java/pl/krasnoludkolo/ebet2/config/WebConfig.java
@@ -9,7 +9,7 @@ class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("/**");
     }
 
 
