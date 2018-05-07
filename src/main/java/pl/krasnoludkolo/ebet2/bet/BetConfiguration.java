c20d693919690d5f79fd7c744854d9835eab4854
janek@projmen.pl <janek@projmen.pl>
2018-05-07 14:46:42 +0200

    Replace jooq repo with spring repo

--- src/main/java/pl/krasnoludkolo/ebet2/bet/BetConfiguration.java
+++ src/main/java/pl/krasnoludkolo/ebet2/bet/BetConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 import pl.krasnoludkolo.ebet2.infrastructure.InMemoryRepository;
 import pl.krasnoludkolo.ebet2.infrastructure.Repository;
+import pl.krasnoludkolo.ebet2.infrastructure.SpringDataRepositoryAdapter;
 import pl.krasnoludkolo.ebet2.user.UserFacade;
 
 import java.util.UUID;
@@ -17,7 +18,7 @@ public class BetConfiguration {
     public BetFacade betFacadeBean(CrudRepository<BetEntity, UUID> repo, UserFacade userFacade) {
         Function<Bet, BetEntity> d2e = Bet::toEntity;
         Function<BetEntity, Bet> e2d = Bet::new;
-        Repository<Bet> repository = new BetJOOQRepository(d2e, e2d);
+        Repository<Bet> repository = new SpringDataRepositoryAdapter<>(repo, d2e, e2d);
         BetManager betManager = new BetManager(repository);
         return new BetFacade(betManager, userFacade);
     }
