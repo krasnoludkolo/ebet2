64fc153bdd5e1b46fd33239e1315dece3e66fcd8
janek@projmen.pl <janek@projmen.pl>
2018-07-19 22:15:01 +0200

    Change id

--- src/main/java/pl/krasnoludkolo/ebet2/results/UserResultEntity.java
+++ src/main/java/pl/krasnoludkolo/ebet2/results/UserResultEntity.java
@@ -1,6 +1,5 @@
 package pl.krasnoludkolo.ebet2.results;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,15 +9,21 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 class UserResultEntity {
 
     @Id
+    private String uuid;
     private String name;
     private int pointCounter;
     @ManyToOne(fetch = FetchType.EAGER)
     private RoundResultsEntity roundResultsEntity;
 
+    UserResultEntity(String name, int pointCounter, RoundResultsEntity roundResultsEntity) {
+        this.name = name;
+        this.pointCounter = pointCounter;
+        this.roundResultsEntity = roundResultsEntity;
+        uuid = roundResultsEntity.getUuid() + "_" + name;
+    }
 }
