37dec76a27a7960ced653559a800689f7e160169
janek@projmen.pl <janek@projmen.pl>
2019-04-11 19:05:54 +0200

    Use ResponseResolver

--- src/main/java/pl/krasnoludkolo/ebet2/results/ResultsController.java
+++ src/main/java/pl/krasnoludkolo/ebet2/results/ResultsController.java
@@ -3,9 +3,8 @@ package pl.krasnoludkolo.ebet2.results;
 import io.vavr.control.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.krasnoludkolo.ebet2.infrastructure.ResponseResolver;
 import pl.krasnoludkolo.ebet2.results.api.LeagueResultsDTO;
 import pl.krasnoludkolo.ebet2.results.api.UserResultDTO;
 
@@ -25,18 +24,14 @@ class ResultsController {
     @GetMapping("/league/{uuid}/results")
     public HttpEntity<LeagueResultsDTO> getResultsForLeague(@PathVariable UUID uuid) {
         Option<LeagueResultsDTO> leagueResultsDTOS = resultFacade.getResultsForLeague(uuid);
-        return leagueResultsDTOS
-                .map(dto -> new ResponseEntity<>(dto, HttpStatus.CREATED))
-                .getOrElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return ResponseResolver.resolve(leagueResultsDTOS);
     }
 
     //TODO paths
     @GetMapping("/results")
     public HttpEntity<UserResultDTO> getResultsFromLeagueToUser(@RequestParam UUID leagueUUID, @RequestParam String user) {
         Option<UserResultDTO> results = resultFacade.getResultsFromLeagueToUser(leagueUUID, user);
-        return results
-                .map(dto -> new ResponseEntity<>(dto, HttpStatus.CREATED))
-                .getOrElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return ResponseResolver.resolve(results);
     }
 
 }
