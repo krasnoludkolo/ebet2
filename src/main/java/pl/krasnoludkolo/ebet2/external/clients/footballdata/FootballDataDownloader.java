06cb8c55068f9e9ef02c94c81d93dc253b13ee52
janek@projmen.pl <janek@projmen.pl>
2019-04-11 23:26:13 +0200

    Refactor

--- src/main/java/pl/krasnoludkolo/ebet2/external/clients/footballdata/FootballDataDownloader.java
+++ src/main/java/pl/krasnoludkolo/ebet2/external/clients/footballdata/FootballDataDownloader.java
@@ -2,17 +2,14 @@ package pl.krasnoludkolo.ebet2.external.clients.footballdata;
 
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import io.vavr.control.Try;
 import org.json.JSONArray;
 import pl.krasnoludkolo.ebet2.external.api.ExternalSourceConfiguration;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 class FootballDataDownloader {
-
-    private static final Logger LOGGER = Logger.getLogger(FootballDataDownloader.class.getName());
 
     private static final String URL_BEGINNING = "http://api.football-data.org/v2/";
     private static final String TOKEN = System.getenv("FOOTBALLDATA_TOKEN");
@@ -24,18 +21,15 @@ class FootballDataDownloader {
     }
 
     JSONArray downloadAllRounds(ExternalSourceConfiguration config) {
-        try {
-            String leagueId = config.getParameter("leagueId");
-            String url = URL_BEGINNING + "competitions/" + leagueId + "/matches";
-            return getMatches(url);
-        } catch (UnirestException e) {
-            LOGGER.log(Level.SEVERE, e.getMessage());
-        }
-        return new JSONArray();
+        String leagueId = config.getParameter("leagueId");
+        String url = URL_BEGINNING + "competitions/" + leagueId + "/matches";
+        return getMatches(url);
     }
 
-    private JSONArray getMatches(String url) throws UnirestException {
-        return getFixturesAsJsonArray(url);
+    private JSONArray getMatches(String url) {
+        return Try.of(() -> getFixturesAsJsonArray(url))
+                .recover(UnirestException.class, new JSONArray())
+                .get();
     }
 
     private JSONArray getFixturesAsJsonArray(String url) throws UnirestException {
