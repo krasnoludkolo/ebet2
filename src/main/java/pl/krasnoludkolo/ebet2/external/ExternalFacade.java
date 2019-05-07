98030a1ced8040a4354169e2b47bd83c6ca285f0
janek@projmen.pl <janek@projmen.pl>
2019-05-07 14:24:21 +0200

    Remove old auto-update mechanism

--- src/main/java/pl/krasnoludkolo/ebet2/external/ExternalFacade.java
+++ src/main/java/pl/krasnoludkolo/ebet2/external/ExternalFacade.java
@@ -17,19 +17,14 @@ public class ExternalFacade {
     private LeagueUpdater leagueUpdater;
     private Map<String, ExternalSourceClient> clientsMap;
     private Repository<LeagueDetails> leagueDetailsRepository;
-    private AutoUpdaterScheduler autoUpdaterScheduler;
 
     ExternalFacade(LeagueInitializer leagueInitializer, LeagueUpdater leagueUpdater, List<ExternalSourceClient> clients, Repository<LeagueDetails> leagueDetailsRepository) {
         this.leagueInitializer = leagueInitializer;
         this.leagueUpdater = leagueUpdater;
         clientsMap = clients.toMap(client -> Tuple.of(client.getShortcut(), client));
         this.leagueDetailsRepository = leagueDetailsRepository;
-        setUpUpdateScheduler();
     }
 
-    private void setUpUpdateScheduler() {
-        autoUpdaterScheduler = new AutoUpdaterScheduler(this);
-    }
 
     public Either<LeagueError, UUID> initializeLeague(ExternalSourceConfiguration config, String clientShortcut, String leagueName) {
         ExternalSourceClient client = clientsMap.get(clientShortcut).getOrElseThrow(IllegalArgumentException::new);
@@ -44,17 +39,6 @@ public class ExternalFacade {
         ExternalSourceClient client = clientsMap.get(leagueDetails.getClientShortcut()).getOrElseThrow(IllegalArgumentException::new);
         LeagueDetails details = leagueUpdater.updateLeague(leagueDetails, client);
         leagueDetailsRepository.update(details.getLeagueUUID(), details);
-    }
-
-    void addLeagueToAutoUpdater(UUID leagueUUID) {
-        checkIfLeagueDetailsExist(leagueUUID);
-        autoUpdaterScheduler.addLeagueToAutoUpdate(leagueUUID);
-    }
-
-    private void checkIfLeagueDetailsExist(UUID leagueUUID) {
-        if (leagueDetailsRepository.findOne(leagueUUID).isEmpty()) {
-            throw new IllegalArgumentException("No league with given uuid");
-        }
     }
 
 }
