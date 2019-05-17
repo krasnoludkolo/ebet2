a6798d7c8edce4d71e351837094d18157b5e6f01
janek@projmen.pl <janek@projmen.pl>
2019-05-17 10:33:42 +0200

    Move NewBet to correct package

--- src/main/java/pl/krasnoludkolo/ebet2/bet/NewBet.java
+++ src/main/java/pl/krasnoludkolo/ebet2/bet/NewBet.java
@@ -1,13 +1,14 @@
-package pl.krasnoludkolo.ebet2.bet.api;
+package pl.krasnoludkolo.ebet2.bet;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import pl.krasnoludkolo.ebet2.bet.api.BetTyp;
 
 import java.util.UUID;
 
 @AllArgsConstructor
 @Getter
-public class NewBet {
+class NewBet {
 
     private final BetTyp betTyp;
     private final UUID matchUUID;
