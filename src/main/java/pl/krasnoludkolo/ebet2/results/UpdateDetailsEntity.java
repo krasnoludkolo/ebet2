04f37f4708564516622521004bd58b071353324a
janek@projmen.pl <janek@projmen.pl>
2019-06-01 20:24:18 +0200

    Set column type

--- src/main/java/pl/krasnoludkolo/ebet2/results/UpdateDetailsEntity.java
+++ src/main/java/pl/krasnoludkolo/ebet2/results/UpdateDetailsEntity.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ class UpdateDetailsEntity {
 
     @Id
     private UUID matchUUID;
+    @Column(columnDefinition = "timestamp without time zone")
     private LocalDateTime scheduledTime;
     private int attempt;
 
