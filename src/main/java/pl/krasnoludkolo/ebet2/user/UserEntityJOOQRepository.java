83279eba9383997317da27ad56e2e5690e73df07
janek@projmen.pl <janek@projmen.pl>
2019-06-11 14:31:02 +0200

    UserEntity JOOQ Repository

---
+++ src/main/java/pl/krasnoludkolo/ebet2/user/UserEntityJOOQRepository.java
@@ -0,0 +1,73 @@
+package pl.krasnoludkolo.ebet2.user;
+
+import org.jooq.*;
+import pl.krasnoludkolo.ebet2.infrastructure.JOOQDatabaseConnector;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.DSL.table;
+
+final class UserEntityJOOQRepository extends JOOQDatabaseConnector<UserEntity, UserEntity> {
+
+    private static final Table<Record> USER_ENTITY = table("user_entity");
+    private static final Field<Object> UUID = field("uuid");
+    private static final Field<Object> USERNAME = field("username");
+    private static final Field<Object> PASSWORD = field("password");
+
+    public UserEntityJOOQRepository(Function<UserEntity, UserEntity> domainToEntityMapper, Function<UserEntity, UserEntity> entityToDomainMapper) {
+        super(domainToEntityMapper, entityToDomainMapper);
+    }
+
+    @Override
+    protected void saveQuery(DSLContext create, UserEntity entity) {
+        create
+                .insertInto(USER_ENTITY)
+                .columns(UUID, USERNAME, PASSWORD)
+                .values(entity.getUuid(), entity.getUsername(), entity.getPassword())
+                .execute();
+    }
+
+    @Override
+    protected Result<Record> findOneQuery(DSLContext create, UUID uuid) {
+        return create
+                .select()
+                .from(USER_ENTITY)
+                .where(UUID.eq(uuid))
+                .limit(1)
+                .fetch();
+    }
+
+    @Override
+    protected UserEntity convertRecordToEntity(Record record) {
+        return modelMapper.map(record, UserEntity.class);
+    }
+
+    @Override
+    protected Result<Record> findAllQuery(DSLContext create) {
+        return create
+                .select().from(USER_ENTITY)
+                .fetch();
+    }
+
+    @Override
+    protected void deleteQuery(DSLContext create, UUID uuid) {
+        create
+                .deleteFrom(USER_ENTITY)
+                .where(field(UUID).eq(uuid))
+                .execute();
+
+    }
+
+    @Override
+    protected void updateQuery(DSLContext create, UserEntity entity, UUID uuid) {
+        create
+                .update(USER_ENTITY)
+                .set(field(USERNAME), entity.getUsername())
+                .set(field(PASSWORD), entity.getPassword())
+                .where(field(UUID).eq(uuid))
+                .execute();
+    }
+
+}
