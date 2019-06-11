ba4951827d7cb5f64a9bf5f0b6d6d2bc9c38542e
janek@projmen.pl <janek@projmen.pl>
2019-06-11 14:37:41 +0200

    Use UserEntity JOOQ Repository

--- src/main/java/pl/krasnoludkolo/ebet2/user/UserConfiguration.java
+++ src/main/java/pl/krasnoludkolo/ebet2/user/UserConfiguration.java
@@ -2,23 +2,19 @@ package pl.krasnoludkolo.ebet2.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.CrudRepository;
 import pl.krasnoludkolo.ebet2.infrastructure.InMemoryRepository;
 import pl.krasnoludkolo.ebet2.infrastructure.Repository;
-import pl.krasnoludkolo.ebet2.infrastructure.SpringDataRepositoryAdapter;
 
-import javax.persistence.EntityManagerFactory;
-import java.util.UUID;
 import java.util.function.UnaryOperator;
 
 @Configuration
 public class UserConfiguration {
 
     @Bean
-    public UserFacade userFacade(CrudRepository<UserEntity, UUID> repo, EntityManagerFactory entityManager) {
+    public UserFacade userFacade() {
         UnaryOperator<UserEntity> d2e = e -> e;
         UnaryOperator<UserEntity> e2d = e -> e;
-        Repository<UserEntity> repository = new SpringDataRepositoryAdapter<>(repo, d2e, e2d, entityManager);
+        Repository<UserEntity> repository = new UserEntityJOOQRepository(d2e, e2d);
         JWTTokenManager tokenManager = new JWTTokenManager();
         PasswordEncrypt passwordEncrypt = new BCryptPasswordEncrypt();
         UserManager userManager = new UserManager(repository, tokenManager, passwordEncrypt);
